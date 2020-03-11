ef0e77c34f676d7232c8fb3570a2f9337fce8448
esq <eqhi1314@163.com>
2020-03-11 10:23:00 +0800

    pom.xml及微服务架构中注册中心核心代码

--- service-eureka/src/main/java/com/cloudedu/serviceeureka/ServiceEurekaApplication.java
+++ service-eureka/src/main/java/com/cloudedu/serviceeureka/ServiceEurekaApplication.java
@@ -2,7 +2,14 @@ package com.cloudedu.serviceeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 微服务-注册中心（Eureka Server）
+ *
+ * @Desc 提供服务注册和发现的能力
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceEurekaApplication {
 
