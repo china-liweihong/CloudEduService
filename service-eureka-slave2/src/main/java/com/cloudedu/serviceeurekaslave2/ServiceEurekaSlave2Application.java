f6b60ae9a4bb49506db14f68a27cea6c1cddb55d
esq <eqhi1314@163.com>
2020-03-12 13:25:28 +0800

    注册中心集群化配置开发及测试

--- service-eureka-slave2/src/main/java/com/cloudedu/serviceeurekaslave2/ServiceEurekaSlave2Application.java
+++ service-eureka-slave2/src/main/java/com/cloudedu/serviceeurekaslave2/ServiceEurekaSlave2Application.java
@@ -1,21 +1,21 @@
-package com.cloudedu.serviceeureka;
+package com.cloudedu.serviceeurekaslave2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * 微服务-注册中心master（Eureka Server）
+ * 微服务-注册中心slave（Eureka Server）
  *
  * @Desc 提供服务注册和发现的能力
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class ServiceEurekaApplication {
+public class ServiceEurekaSlave2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceEurekaApplication.class, args);
-
-        System.out.println("注册中心-master启动成功，请接着启动注册中心-slave1");
+        SpringApplication.run(ServiceEurekaSlave2Application.class, args);
+        System.out.println("注册中心-slave2启动成功，请接着启动配置中心(service-config)");
     }
+
 }
